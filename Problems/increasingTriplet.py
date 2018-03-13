2c2d85041f02253b33bd6978a5d059f1254553ee
tylors1 <tylors1@gmail.com>
2018-03-13 16:39:35 -0700

    increasingTriplet

---
+++ Problems/increasingTriplet.py
@@ -0,0 +1,21 @@
+def increasingTriplet(nums):
+	low = mid = float('inf')
+
+	for num in nums:
+		if num < low:
+			low = num
+		if low < num < mid:
+			mid = num
+		if num > mid:
+			return True
+	return False
+
+
+
+
+
+
+
+nums = [6,7,1,2]
+# nums = [6,7,1,2,8]
+print increasingTriplet(nums)
