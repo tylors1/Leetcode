ba8cff26f44f27e90bd6581512b4f78a92c51606
tylors1 <tylors1@gmail.com>
2018-03-15 19:02:11 -0700

    daily problemset

---
+++ Problems/combinations.py
@@ -0,0 +1,28 @@
+
+
+def combinations(n, c):
+	res = []
+
+	def recurse(start, combos):
+		if len(combos) == c:
+			res.append(combos[:])
+			return
+
+		# if start > n:
+		if len(combos) + (n - start + 1) < c:
+			return
+
+		combos.append(start)
+		recurse(start + 1, combos)
+		combos.pop()
+		recurse(start + 1, combos)
+
+		pass
+
+	recurse(1, [])
+	return res
+
+c = 2
+n = 5
+
+print combinations(n, c)
