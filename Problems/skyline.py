612b583bf8c0e0781190b50b0667e5159f426200
tylors1 <tylors1@gmail.com>
2018-04-04 17:01:37 -0700

    city skylines

---
+++ Problems/skyline.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/max-increase-to-keep-city-skyline/description/
+
+def skyline(grid):
+# get max of each row/col
+# each item of res is min of the max row/col
+
+	res = 0
+	cols = []
+	rows = []
+	for col in zip(*grid):
+		cols.append(max(col))
+	for row in grid:
+		rows.append(max(row))
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			# grid[i][j] = min(cols[j], rows[i])
+			res += min(cols[j], rows[i]) - grid[i][j]
+	return res
+
+
+grid = [ [3, 0, 8, 4], 
+  		[2, 4, 5, 7],
+  		[9, 2, 6, 3],
+  		[0, 3, 1, 0] ]
+
+print skyline(grid)
