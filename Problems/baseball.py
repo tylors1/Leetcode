d0f2364969e7eef5dd6365d48342285ef4f68f0d
tylors1 <tylors1@gmail.com>
2018-05-10 13:11:40 -0700

    baseball

--- Problems/baseball.py
+++ Problems/baseball.py
@@ -1,40 +1,25 @@
 
-# keeping track of:
-# total sum
-# two pointers/values:
-# 	valid play 1
-# 	valid play 2
-
 
 def calPoints(ops):
 
-	total = 0
-	stack = []
+	st = []
+	curr = 0
 
-	for i in range(len(ops)):
-		print stack
-		if ops[i] == "+":
-			stack.append(stack[-1] + stack[-2])
-			total += stack[-1]
-			# print "add ", stack[-1]
-		elif ops[i] == "C":
-			# print "subtract ", stack[-1]
-			total -= stack.pop()
-		elif ops[i] == "D":
-			stack.append(stack[-1] * 2)
-			# print "add ", stack[-1]
-			total += stack[-1]
+	for op in ops:
+		if op == "C":
+			curr -= st.pop()
+		elif op == "D":
+			curr += st[-1]*2
+			st.append(st[-1]*2)
+		elif op == "+":
+			curr += st[-1]+st[-2]
+			st.append(st[-1]+st[-2])
 		else:
-			stack.append(int(ops[i]))
-			total += stack[-1]
-	return total
+			curr += int(op)
+			st.append(int(op))
+	return curr
 
 
-
-
-
-
-
-# lol = ["5","2","C","D","+"]
-lol = ["5","-2","4","C","D","9","+","+"]
-print calPoints(lol)
+ops = ["5","2","C","D","+"]
+ops = ["5","-2","4","C","D","9","+","+"]
+print calPoints(ops)
